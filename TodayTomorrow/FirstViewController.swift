e3be11922edc64667a11b49c66259026ce638483
Sergey Kozak <svkozak@icloud.com>
2017-12-20 21:16:52 -0500

    Initial Commit

---
+++ TodayTomorrow/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TodayTomorrow
+//
+//  Created by Sergey Kozak on 20/12/2017.
+//  Copyright © 2017 Centennial. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
