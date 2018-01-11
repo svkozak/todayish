194d4948580ec7b3c8a18f0bc55f9dcd3e73116c
Sergey Kozak <svkozak@icloud.com>
2018-01-11 00:36:13 -0500

    Added SomeDay viewcontroller

--- TodayTomorrow/SomeDayTaskCell.swift
+++ TodayTomorrow/SomeDayTaskCell.swift
@@ -10,14 +10,43 @@ import UIKit
 
 class SomeDayTaskCell: UITableViewCell {
 
+    let someDayBlue = UIColor(red: 0.161, green: 0.502, blue: 0.725, alpha: 1)
+    
+    
+    @IBOutlet weak var todayTaskNameLabel: UILabel!
+    @IBOutlet weak var checkBox: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var bottomBorder: UIView!
+    
+    
+    func checkBoxCheck() {
+        print("pressed")
+    }
+    
+    func setChecked () {
+        checkBox.setImage(UIImage(named: "grey-selected"), for: UIControlState.normal)
+        todayTaskNameLabel.textColor = UIColor.lightGray
+        descriptionLabel.textColor = UIColor.lightGray
+        bottomBorder.backgroundColor = UIColor.lightGray
+    }
+    
+    func setUnchecked() {
+        checkBox.setImage(UIImage(named: "green-deselected"), for: UIControlState.normal)
+        todayTaskNameLabel.textColor = someDayBlue
+        descriptionLabel.textColor = someDayBlue
+        bottomBorder.backgroundColor = someDayBlue
+    }
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
 
--- TodayTomorrow/TomorrowVC.swift
+++ TodayTomorrow/TomorrowVC.swift
@@ -11,28 +11,63 @@ import CoreData
 
 class TomorrowVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var tasks: [Task] = []
     
-    
-    
-    
-    
-    
+    @IBOutlet weak var tableView: UITableView!
     
     
     
     // MARK: ---- TableView Implementation
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return tasks.count
+    }
+    
+    func setSelectedCell(cell: SomeDayTaskCell, checked: Bool) {
+        if checked {
+            cell.setChecked()
+        } else{
+            cell.setUnchecked()
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "someDayTaskCell") as! SomeDayTaskCell
+        cell.todayTaskNameLabel.text = tasks[indexPath.row].taskName
+        
+        setSelectedCell(cell: cell, checked: tasks[indexPath.row].isCompleted)
+        
+        if tasks[indexPath.row].taskDescription == "Task description" {
+            cell.descriptionLabel.text = ""
+        } else {
+            cell.descriptionLabel.text = tasks[indexPath.row].taskDescription
+        }
         return cell
     }
     
     
+
     
+    // Get data from database
+    
+    func getData() {
+        let fetchRequest = NSFetchRequest<Task>(entityName: "Task")
+        let sort = NSSortDescriptor(key: #keyPath(Task.isCompleted), ascending: true)
+        let predicate = NSPredicate(format: "dueToday == FALSE")
+        fetchRequest.predicate = predicate
+        fetchRequest.sortDescriptors = [sort]
+        do {
+            tasks = try context.fetch(fetchRequest)
+        } catch {
+            print("Cannot fetch")
+        }
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getData()
+    }
     
     
     
